84df602de7bbc00fd8d03e5707a5a8f1754c0b2a
CapslockJesus <Daniel.Reichert.1991@gmx.de>
2018-08-13 11:26:13 +0200

    Amazon Keys ausgelagert und included.

--- assets/logic/mAmazonAccesKeys.php
+++
@@ -1,8 +0,0 @@
-<?php
-        
-include("mCon.php");
-        
-$dbh = new PDO ("sqlsrv:Server=$hostname;Database=$dbname","$dbusername","$pw");
-        
-$sql = "SELECT SellerID, MWSAuthToken, AWSAccesKeyID, SecretKey, MarketplaceID, Sellername
-                FROM tAmazonAccess";
